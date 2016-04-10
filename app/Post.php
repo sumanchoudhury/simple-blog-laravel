19313d3b62b5701afb8283060ae67749a468b9a8
Suman Choudhury <suman.17biz@gmail.com>
2016-04-10 22:22:56 +0530

    Post model

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $table = 'posts';
+
+    protected $fillable = array('title','content','author_id');
+
+    public $timestamps = true;
+
+    public function Author(){
+
+        return $this->belongsTo('User','author_id');
+    }
 }
